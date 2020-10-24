8bc834f274e243330553f65ff75ea043febc009f
manuelineco <59016245+manuelineco@users.noreply.github.com>
2020-10-25 00:14:17 +0200

    Add files via upload

---
+++ data/PuntosQR_1.js
@@ -0,0 +1,15 @@
+var json_PuntosQR_1 = {
+"type": "FeatureCollection",
+"name": "PuntosQR_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "Sitio": "Casita", "Pista": "A buscar, por desgracia no soy un sobao, me encontrarías demasiado rápido ;)" }, "geometry": { "type": "Point", "coordinates": [ -3.669364893544056, 40.427124121879828 ] } },
+{ "type": "Feature", "properties": { "fid": "2", "Sitio": "Gim", "Pista": "La respuesta a donde estoy la tienen tus compañeros" }, "geometry": { "type": "Point", "coordinates": [ -3.665472452321629, 40.427181691573907 ] } },
+{ "type": "Feature", "properties": { "fid": "3", "Sitio": "Pista", "Pista": "Estoy cabezabajo" }, "geometry": { "type": "Point", "coordinates": [ -3.665260017638466, 40.427012180230427 ] } },
+{ "type": "Feature", "properties": { "fid": "4", "Sitio": "Fuente del Berro", "Pista": "La esquina, estoy en la esquina" }, "geometry": { "type": "Point", "coordinates": [ -3.66090216066803, 40.428898293730491 ] } },
+{ "type": "Feature", "properties": { "fid": "5", "Sitio": "Parque", "Pista": "Piensa en verde" }, "geometry": { "type": "Point", "coordinates": [ -3.669080238644844, 40.429968013065519 ] } },
+{ "type": "Feature", "properties": { "fid": "6", "Sitio": "Wizink", "Pista": "Estoy pegada en algún mobiliario por aquí" }, "geometry": { "type": "Point", "coordinates": [ -3.672582091218774, 40.423546658120614 ] } },
+{ "type": "Feature", "properties": { "fid": "7", "Sitio": "Pepita", "Pista": "Me gusta asomarme para ver a la gente" }, "geometry": { "type": "Point", "coordinates": [ -3.668698480138026, 40.427461563935886 ] } },
+{ "type": "Feature", "properties": { "fid": "8", "Sitio": "Parada Manuel Becerra", "Pista": "Es la última, no hay pista que valga" }, "geometry": { "type": "Point", "coordinates": [ -3.669275164820553, 40.427613642365614 ] } }
+]
+}
